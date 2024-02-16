80e5535b7b50e43368abfdc9be860a6a57e1ca3f
phtrebil <phptrebil@gmail.com>
2024-02-16 17:50:59 -0300

    dando função aos botões

--- app/src/main/java/com/example/cupcake/CupcakeScreen.kt
+++ app/src/main/java/com/example/cupcake/CupcakeScreen.kt
@@ -40,6 +40,7 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.example.cupcake.data.DataSource
+import com.example.cupcake.ui.OrderSummaryScreen
 import com.example.cupcake.ui.OrderViewModel
 import com.example.cupcake.ui.SelectOptionScreen
 import com.example.cupcake.ui.StartOrderScreen
@@ -96,6 +97,7 @@ fun CupcakeApp(
             composable(route = CupcakeRoute.Start.name) {
                 StartOrderScreen(
                     quantityOptions = DataSource.quantityOptions,
+                    onNextButtonClicked = {},
                     modifier = Modifier
                         .fillMaxSize()
                         .padding(dimensionResource(R.dimen.padding_medium))
@@ -119,7 +121,13 @@ fun CupcakeApp(
                     onSelectionChanged = {viewModel.setDate(it)},
                     modifier = Modifier.fillMaxHeight()
                 )
+            }
 
+            composable(route = CupcakeRoute.Summary.name) {
+                OrderSummaryScreen(
+                    orderUiState = uiState,
+                    modifier = Modifier.fillMaxHeight()
+                )
             }
 
         }
